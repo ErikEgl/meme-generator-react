fc08b0329f13ef48acc77f78122abdbe0d886e44
Erik Egliens <erik@bellcom.ee>
2022-10-24 16:23:24 +0300

    react draggable with styles

--- src/components/Meme/Meme.js
+++ src/components/Meme/Meme.js
@@ -1,19 +1,22 @@
-import React from "react";
-// import memesData from "./memesData";
-
+import { useRef, useState, useEffect } from "react";
+import Draggable from "react-draggable";
 export default function Meme() {
-  const [meme, setMeme] = React.useState({
+  const [meme, setMeme] = useState({
     topText: "",
     bottomText: "",
     randomImage: "http://i.imgflip.com/1bij.jpg",
   });
-  const [allMemes, setAllMemes] = React.useState({});
-
-  React.useEffect(() => {
+  const [allMemes, setAllMemes] = useState({});
+  const ref = useRef(null);
+  useEffect(() => {
     fetch("https://api.imgflip.com/get_memes")
-      .then((response) =>  response.json())
-      .then(data => setAllMemes(data.data.memes));
+      .then((response) => response.json())
+      .then((data) => setAllMemes(data.data.memes));
   }, []);
+
+  useEffect(() => {
+    console.log("width", ref.current ? ref.current.offsetWidth : 0);
+  }, [ref.current, meme]);
 
   function handleChange(event) {
     const { name, value } = event.target;
@@ -34,6 +37,18 @@ export default function Meme() {
       randomImage: url,
     }));
   }
+  const [deltaPosition, setDeltaPosition] = useState({x: 0, y: 0});
+  const [rotation, setRotation] = useState(0);
+  const handleDrag = (e, ui) => {
+    const { x, y } = deltaPosition;
+
+    setDeltaPosition({
+        x: x + ui.deltaX,
+        y: y + ui.deltaY,
+    });
+
+    setRotation(rotation + ui.deltaX / 20);
+};
 
   return (
     <>
@@ -42,7 +57,7 @@ export default function Meme() {
           <input
             type="text"
             placeholder="Top text"
-            className="form--input"
+            className="form--input target"
             name="topText"
             onChange={handleChange}
             value={meme.topText}
@@ -59,11 +74,25 @@ export default function Meme() {
             Get a new meme image 🖼
           </button>
         </div>
-        <div className="meme">
-          <img src={meme.randomImage} className="meme--image" />
-          <h2 className="meme--text top">{meme.topText}</h2>
-          <h2 className="meme--text bottom">{meme.bottomText}</h2>
-        </div>
+        <div style={{ width: ref.current?.offsetWidth }} className="meme">
+          <img
+            ref={ref}
+            alt="Text"
+            src={meme.randomImage}
+            className="meme--image"
+          />
+          {meme.topText.length > 0 && (<Draggable bounds={"parent"}>
+            <h2 style={{ maxWidth: ref.current?.offsetWidth }} className="meme--text top">
+                  {meme.topText}
+            </h2>
+          </Draggable>)}
+ 
+          {meme.bottomText.length > 0 && (<Draggable bounds={"parent"}>
+            <h2 style={{ maxWidth: ref.current?.offsetWidth }} className="meme--text bottom">
+              {meme.bottomText}
+            </h2>
+          </Draggable>)}
+          </div>
       </main>
     </>
   );
